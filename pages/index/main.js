8df239717460921db63acf190a2d4bda07fcb92b
unknown <1546700626@qq.com>
2019-06-25 17:41:32 +0800

    更新代码

--- pages/index/main.js
+++ pages/index/main.js
@@ -4,15 +4,29 @@ const app = getApp()
 
 Page({
   data: {
-    motto: '欢迎登入！This is my Tardis!',
+    avatarUrl: null,
+    imgUrls: [{url:'../../image/doctor1.jpg'},
+              {url:'../../image/doctor2.jpg'},
+              {url:'../../image/doctor3.jpg'},
+              {url:'../../image/doctor4.jpg'}],
+    autoplay: true,
+    interval: 3000,
+    duration: 1200,
+    src: '',
     userInfo: {},
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo')
   },
+
   //事件处理函数
   bindViewTap: function () {
     wx.navigateTo({
       url: '../logs/logs'
+    })
+  },
+  gotoPage: function (e) {
+    wx.navigateTo({
+      url: '../index/main'
     })
   },
   onLoad: function () {
@@ -50,6 +64,39 @@ Page({
       userInfo: e.detail.userInfo,
       hasUserInfo: true
     })
+  },
+  /**
+  * 打开本地视频
+  */
+  bindButtonTap: function () {
+    var that = this
+    //拍摄视频或从手机相册中选视频
+    wx.chooseVideo({
+      //album 从相册选视频，camera 使用相机拍摄，默认为：['album', 'camera']
+      sourceType: ['album', 'camera'],
+      //拍摄视频最长拍摄时间，单位秒。最长支持60秒
+      maxDuration: 60,
+      //前置或者后置摄像头，默认为前后都有，即：['front', 'back']
+      camera: ['front', 'back'],
+      //接口调用成功，返回视频文件的临时文件路径，详见返回参数说明
+      success: function (res) {
+        console.log(res.tempFilePaths[0])
+        that.setData({
+          src: res.tempFilePaths[0]
+        })
+      }
+    })
+  },
+  /**
+   * 当发生错误时触发error事件，event.detail = {errMsg: 'something wrong'}
+   */
+  videoErrorCallback: function (e) {
+    console.log('视频错误信息:')
+    console.log(e.detail.errMsg)
   }
+    
+  
+
+ 
 
 })
